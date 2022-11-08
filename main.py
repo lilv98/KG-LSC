6e5f7121a1a3e30051755e354ec9dab889801d97
lilv98 <zhenwei.tang@kaust.edu.sa>
2022-11-08 05:39:00 +0300

    read_data

--- main.py
+++ main.py
@@ -4,39 +4,42 @@ import tqdm
 
 def get_words():
     words = set()
-    with open('./corpus1/lemma/ccoha1.txt') as f:
+    with open('./ccoha1.txt') as f:
         for line in f:
             line = line.strip().split(' ')
             for i in range(len(line)):
                 words.add(line[i])
-    with open('./corpus2/lemma/ccoha2.txt') as f:
+    with open('./ccoha2.txt') as f:
         for line in f:
             line = line.strip().split(' ')
             for i in range(len(line)):
                 words.add(line[i])
     return {v: k for k, v in enumerate(words)}
-    
-    # with open('./corpus' + str(id) + '/lemma/ccoha' + str(id) + '.txt') as f:
-    #     for line in tqdm.tqdm(f):
-    #         line = line.strip().split(' ')
-    #         for i in range(len(line) - 1):
-    #             triple = (words[line[i]], 0, words[line[i + 1]])
-    #             if triple in triples:
-    #                 triples[triple] += 1
-    #             else:
-    #                 triples[triple] = 1
-    
-    # data = []
-    # for v, k in enumerate(triples):
-    #     data.append([k[0], k[1], k[2], v])
-    # data = torch.tensor(data)
-    
-    # return words, triples, data
 
+def get_cooccur(words, id):
+    triples = {}
+    with open('./ccoha' + str(id) + '.txt') as f:
+        for line in tqdm.tqdm(f):
+            line = line.strip().split(' ')
+            for i in range(len(line) - 1):
+                triple = (words[line[i]], 0, words[line[i + 1]])
+                if triple in triples:
+                    triples[triple] += 1
+                else:
+                    triples[triple] = 1
+    
+    data = []
+    for _, k in enumerate(triples):
+        data.append([k[0], k[1], k[2], triples[k]])
+    data = torch.tensor(data)
+    
+    return triples, data
 
 
 if __name__ == '__main__':
-    words_1, triples_1, data_1 = read_corpus(id=1)
-    words_2, triples_2, data_2 = read_corpus(id=2)
+    e_dict = get_words()
+    triples_1, data_1 = get_cooccur(e_dict, id=1)
+    triples_2, data_2 = get_cooccur(e_dict, id=2)
+    triples_common = torch.tensor(list(set(triples_1) & set(triples_2)))
     pdb.set_trace()
     
